8fda5509fc874b2ca9f8cedd066838d11c51512f
AD Dahal <152646542+J9LLY9@users.noreply.github.com>
2026-08-19 11:36:44 -0700

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        map = {}
+        for word in strs:
+            key = "".join(sorted(word))
+            map[key] = map.get(key, []) + [word]
+        return list(map.values())
+
